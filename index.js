376b03a27db484eee2ad97bdf598f0d99d8d6844
ddelgado25 <118149533+ddelgado25@users.noreply.github.com>
2023-05-30 01:33:48 -0400

    created add department function

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const questions = async () => {
                     viewAllEmployees();
                     break;
                 case "Add a Department":
-                    addDept();
+                    addDepartment();
                     break;
                 case "Add a Role":
                     addRole();
@@ -51,7 +51,7 @@ const questions = async () => {
         })
 };
 
-// Functiion allows user to view all departments when selected on main menu
+// Function allows user to view all departments when selected on main menu
 function viewAllDepartments() {
     db.query("SELECT * FROM department", (err, res) => {
         if (err) throw (err);
@@ -59,7 +59,7 @@ function viewAllDepartments() {
     });
 }
 
-// Functiion allows user to view all roles when selected on main menu
+// Function allows user to view all roles when selected on main menu
 function viewAllRoles() {
     db.query("SELECT * FROM role", (err, res) => {
         if (err) throw (err);
@@ -67,7 +67,7 @@ function viewAllRoles() {
     });
 }
 
-// Functiion allows user to view all employees when selected on main menu
+// Function allows user to view all employees when selected on main menu
 function viewAllEmployees() {
     db.query("SELECT employee.id, employee.first_name, employee.last_name, employee.role_id, employee.manager_id, role.title, role.salary, role.id, department.id FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id", (err, res) => {
       if (err) throw (err);
@@ -75,3 +75,21 @@ function viewAllEmployees() {
     
     }
   )};
+
+// Function allows user to add a department when selected on main menu
+  const addDepartment = async () => {
+    await inquirer.prompt (
+      {
+          type: 'input',
+          name: 'newDeptartment',
+          message: 'What is the name of the new Department you would like to add?'
+  
+      })
+  
+      .then(function(deptartmentName){
+        db.query("INSERT INTO department (name) VALUES (?)", [deptartmentName.newDeptartment], (err, res) => {
+          if (err) throw (err);
+          console.table(res);
+  
+      })})
+  };
